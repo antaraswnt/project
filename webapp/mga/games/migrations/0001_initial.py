0e023ee17d05938923b1ef7c6df88174b2b3535a
Nishant Shelar <nishant8887@gmail.com>
2015-04-11 16:45:42 +0530

    Removed all unwanted migrations.

--- webapp/mga/games/migrations/0001_initial.py
+++ webapp/mga/games/migrations/0001_initial.py
@@ -16,8 +16,12 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=100)),
                 ('slug', models.CharField(max_length=30)),
+                ('description', models.CharField(max_length=255, null=True, blank=True)),
                 ('display_url', models.URLField()),
                 ('control_url', models.URLField()),
+                ('is_active', models.BooleanField(default=False)),
+                ('large_image_url', models.URLField(null=True, blank=True)),
+                ('small_image_url', models.URLField(null=True, blank=True)),
             ],
             options={
             },
